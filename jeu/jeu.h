9458badc6aaa584c261c388d65903a65f0f9aed7
Michał Ptak <michal.ptak@epfl.ch>
2025-03-18 16:13:48 +0100

    first commit

--- jeu/jeu.h
+++ jeu/jeu.h
@@ -3,5 +3,18 @@
 //
 #pragma once
 #include <debug/debug.h>
+#include "../tools/tools.h"
+
+struct information {
+    int score;
+    int nbParticule;
+
+
+};
+
+information read_file(){
+
+};
+
 
 
