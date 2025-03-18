f30ed8438cd8bcd4fc4e9be2c3ad47591e23a852
Michał Ptak <michal.ptak@epfl.ch>
2025-03-18 17:39:53 +0100

    Started defining readfile function

--- jeu/jeu.h
+++ jeu/jeu.h
@@ -4,7 +4,9 @@
 #pragma once
 #include <debug/debug.h>
 #include "../tools/tools.h"
+#include "../message/message.h"
 #include <vector>
+#include <string>
 
 
 
@@ -16,11 +18,13 @@ struct information {
     std::vector<Node> Faiseurs;
     int nbFaiseurs;
 
-
-
 };
 
-information read_file(){
+information read_file(const std::string& filename){
+    std::ifstream file(filename);
+    if (+file.is_open()){
+
+    }
 
 
 };
