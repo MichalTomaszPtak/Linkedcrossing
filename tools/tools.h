81a87203a5cca028143c781c55df926878f01767
isa <uanpis@duck.com>
2025-03-22 13:08:50 +0100

    Revert "polar stuff"
    This reverts commit 37e6172670ce1403e4455def603419aaf64e44ae.

--- tools/tools.h
+++ tools/tools.h
@@ -8,20 +8,13 @@ typedef struct S2d {
 public:
 	float x;
 	float y;
-
 	S2d(void);
 	S2d(float a, float b);
-
-	void set_polar(float r, float angle);
-
 	void scale(float factor);
 	void add(S2d a);
 	void addscaled(S2d a, float factor);
 	void sub(S2d a);
-	void reflect(S2d axis);
-
 	float dot(S2d a);
-	float get_angle(void);
 	float get_length(void);
 	float get_length_squared(void);
 private:
