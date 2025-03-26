61bea2296ccfb7dd67160129e3e38d01f10ba12e
isa <uanpis@duck.com>
2025-03-26 13:48:23 +0100

    fix

--- testall.sh
+++ testall.sh
@@ -3,6 +3,7 @@
 #   This script runs all the test files inside ./public and checks
 #   the output against the provided expected output.
 
+EXEC="./projet"
 PUBLIC_DIR="./public"
 for input_file in "$PUBLIC_DIR"/t*.txt; do
     base_name=$(basename "$input_file")
@@ -11,9 +12,8 @@ for input_file in "$PUBLIC_DIR"/t*.txt; do
 
     if [[ -f "$output_file" ]]; then
         echo "Running test for $input_file..."
-        ./main.elf "$input_file" \
-            | sed -e 's/ /␣/g' -e $'s/\t/⟶/g' \
-            | diff -s - <(cat "$output_file" | sed -e 's/ /␣/g' -e $'s/\t/⟶/g')
+        $EXEC "$input_file" \
+            | diff -s - <(cat "$output_file")
     else
         echo "Warning: Output file $output_file does not exist for $input_file"
     fi
